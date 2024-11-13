a034e94db24ae9bc234eca58c461f70059b68fae
unknown-northKorean-man <600935@buchananschools.com>
2024-11-13 11:38:41 -0500

    Background color

--- ex02/script.js
+++ ex02/script.js
@@ -1,6 +1,6 @@
 // Write your Task 1 code inside this function!
 function task1() {
-  console.log(target);
+  console.log("target");
   //target div
   let target = document.querySelector("#text-content")
   //set inertext to somthing new
@@ -10,8 +10,10 @@ function task1() {
 // Write your task 2 code inside this function!
 function task2() {
   console.log("Running task 2");
-  let target = document.querySelector("#code")
-  console.log("")
+  let target = document.querySelector("#background-color")
+  function setBgRed(){
+    document.body.style.backgroundColor ='Red'
+  }
 }
 
 // Write your task 3 code inside this function!
