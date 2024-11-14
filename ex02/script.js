c6b632c2336b6cd632b3f72e035aff4cad777d2f
unknown-northKorean-man <600935@buchananschools.com>
2024-11-14 12:09:23 -0500

    fixed

--- ex02/script.js
+++ ex02/script.js
@@ -1,22 +1,32 @@
 // Write your Task 1 code inside this function!
 function task1() {
+  document.querySelector("code")
   console.log("target");
   //target div
   let target = document.querySelector("#text-content")
   //set inertext to somthing new
-  target.innerText = "somthing-new"
+  target.innerText = "somthing new"
 }
 
 // Write your task 2 code inside this function!
 function task2() {
   console.log("Running task 2");
-  let target = document.querySelector("#background-color")
-  function setBgRed(){
-    document.body.style.backgroundColor ='Red'
-  }
+  let button = document.querySelector("#red")
+  let html = document.querySelector('#task2')
+  let inverted = false;
+  html.classList.toggle("inverted");
+
+  inverted = !inverted;
 }
 
 // Write your task 3 code inside this function!
 function task3() {
-  console.log("Running task 3");
+  console.log("Running task 3")
+}
+
+function task3() {
+  var ul = document.getElementById("list");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode("New Task"));
+  ul.appendChild(li);
 }
