ec77133d9d1b64e15005428575c2933e917b9d0b
unknown-northKorean-man <600935@buchananschools.com>
2024-11-18 11:58:56 -0500

    it works

--- code-along/script.js
+++ code-along/script.js
@@ -1,1 +1,13 @@
 console.log("I am here")
+let myArray = ["me",
+    "my-self"
+    ,"I"]
+
+let names = document.querySelectorAll("li")
+let targrt = document.querySelector("receiver")
+
+names.forEach(item => {
+    let newEl= document.createElement("li")
+    newEl.innerText = item;
+
+})
